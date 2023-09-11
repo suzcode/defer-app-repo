9115cd8d50dde4b315349efc2cd818952ae64158
suzcode <suzetteloomb@gmail.com>
2023-09-12 07:12:44 +1200

    added python slim to dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
@@ -21,5 +20,4 @@ def ping_pong():
 
 
 if __name__ == '__main__':
-    from waitress import serve
     app.run(host='0.0.0.0', port=8080)
