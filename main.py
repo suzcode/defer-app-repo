d98ea022aab13222486efa654fad302fd469f125
suzcode <suzetteloomb@gmail.com>
2023-10-04 08:27:39 +1300

    added firestore

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")
+db = firestore.client()
+firebase_admin.initialize_app(cred)
 
 
 # instantiate the app
@@ -16,8 +23,10 @@ def main():
 # sanity check route
 @app.route('/microservice1', methods=['GET'])
 def ping_pong():
+    docs = db.collection("users").stream()
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
     return jsonify('pong!')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
