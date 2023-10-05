85d0857a95a800329c77863fd25ac3c45b74f50d
suzcode <suzetteloomb@gmail.com>
2023-10-06 06:23:37 +1300

    changed initialize_app(cred) to ()

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials
 
 cred = credentials.Certificate("serviceAccountKey.json")
 db = firestore.client()
-app = firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app()
 
 
 # instantiate the app
