57da6b2f538475d2feec551d046873712db5f0d6
suzcode <suzetteloomb@gmail.com>
2023-10-07 05:34:38 +1300

    updated serviceAccountKey

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 
 cred = credentials.Certificate("serviceAccountKey.json")
-db = firestore.client()
-firebase_admin.initialize_app()
 
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 # instantiate the app
 app = Flask(__name__)
