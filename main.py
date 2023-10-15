126d140e8f144e44f468eeaa4920a7cc53b306de
suzcode <suzetteloomb@gmail.com>
2023-10-16 05:21:05 +1300

    removed app assignment

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
 def ping_pong():
     api_key = get_api_key()
     credentials = firebase_admin.credentials.Certificate(json.loads(api_key))
-    app = firebase_admin.initialize_app(credentials=credentials)
+    firebase_admin.initialize_app(credentials=credentials)
     data = []
     docs = db.collection("users").stream()
     for doc in docs:
