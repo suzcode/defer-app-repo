888793a6aec0acde41ca6f977b01052be2082335
suzcode <suzetteloomb@gmail.com>
2023-10-15 21:09:15 +1300

    updated api code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import firebase_admin
 from firebase_admin import firestore
 from google.cloud import secretmanager
+import json
 
 
 firebase_admin.initialize_app()
@@ -22,8 +23,6 @@ def get_api_key():
     response = client.access_secret_version(request={"name": secret_name})
     return response.payload.data.decode("UTF-8")
 
-api_key = get_api_key()
-
 
 @app.route('/')
 def main():
@@ -32,10 +31,17 @@ def main():
 # sanity check route
 @app.route('/microservice1', methods=['GET'])
 def ping_pong():
+    api_key = get_api_key()
+    credentials = firebase_admin.credentials.Certificate(json.loads(api_key))
+    app = firebase_admin.initialize_app(credentials=credentials)
+    data = []
     docs = db.collection("users").stream()
     for doc in docs:
-        print(f"{doc.id} => {doc.to_dict()}")
-    return jsonify(doc.id)
+        data.append({
+            "id": doc.id,
+            "data": doc.to_dict()
+        })
+    return jsonify({"data": data})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
