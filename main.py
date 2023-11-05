8bf22ba49411860dac27656d5c65854c8874b5c1
suzcode <suzetteloomb@gmail.com>
2023-11-05 15:08:42 +1300

    added import json, request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, json, request
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import firestore
