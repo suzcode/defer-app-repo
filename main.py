f7cf841d54d7eb45d2cb7eb34aa675eb04b9184d
suzcode <suzetteloomb@gmail.com>
2023-11-07 05:35:53 +1300

    changed header to application/json

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def after_request(response):
     if request.headers['Origin'] in white_origin:
         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,application/json'
     return response
 
 @app.route('/microservice1', methods=['GET', 'POST'])
