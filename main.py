8f83c46af668eca15e70abef54b2f2ace14dff7a
suzcode <suzetteloomb@gmail.com>
2023-11-26 11:20:16 +1300

    added if 'Origin' in request.headers:

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify, render_template, json, request
-from flask_cors import CORS
 import firebase_admin
 from firebase_admin import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-# updated 26 Nov
-# instantiate the app
+
 app = Flask(__name__)
 
 app.config.from_object(__name__)
@@ -14,30 +12,18 @@ firebase_admin.initialize_app()
 
 db = firestore.client()
 
-# CORS(app, resources={r'/microservice1': {'origins': '*'}})
-
 @app.route('/')
 def main():
     return render_template("defrr.html")
 
-# sanity check routes
-# @app.route('/microservice1', methods=['GET'])
-# def ping_pong():
-#     users_ref = db.collection('users')
-#     field_filter = FieldFilter("age", "==", 54)
-#     query = users_ref.where(filter=field_filter)  # Your query conditions here
-#     results = query.stream()
-#     users_data = []
-#     for result in results:
-#         users_data.append({"id": result.id, "data": result.to_dict()})
-#     return jsonify(users_data)
-
 @app.after_request
 def after_request(response):
     white_origin= [r'/microservice1','http://34.111.111.147']
-    if request.headers['Origin'] in white_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
-        # response.headers['Access-Control-Allow-Headers'] = 'Content-Type,application/json'
+
+    # Check if 'Origin' is present in request headers
+    if 'Origin' in request.headers:
+        if request.headers['Origin'] in white_origin:
+            response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
     return response
 
 @app.route('/microservice1', methods=['GET', 'POST'])
