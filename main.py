18023848477374b912e6d59f79ceaca6132db200
suzcode <suzetteloomb@gmail.com>
2023-11-28 07:00:02 +1300

    return users_data

--- main.py
+++ main.py
@@ -38,17 +38,16 @@ def user_details():
             print('Post data raw', post_data_raw)
             post_data = request.data
         print('Post data is:', post_data)
-        return post_data
-        # ADD BACK IN
-        # user_age = post_data['ageval']
-        # users_ref = db.collection('users')
-        # field_filter = FieldFilter("age", "==", user_age)
-        # query = users_ref.where(filter=field_filter)  # Your query conditions here
-        # results = query.stream()
-        # users_data = []
-        # for result in results:
-        #     users_data.append({"id": result.id, "data": result.to_dict()})
-        # return jsonify(users_data)
+        # return post_data
+        user_age = post_data['ageval']
+        users_ref = db.collection('users')
+        field_filter = FieldFilter("age", "==", user_age)
+        query = users_ref.where(filter=field_filter)  # Your query conditions here
+        results = query.stream()
+        users_data = []
+        for result in results:
+            users_data.append({"id": result.id, "data": result.to_dict()})
+        return jsonify(users_data)
     else:
         users_data = ["hell0"]
         print(users_data)
