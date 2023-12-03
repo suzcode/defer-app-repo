0227363aa9d7b341b2f9519c8c5d25645b65ff68
suzcode <suzetteloomb@gmail.com>
2023-12-03 21:11:57 +1300

    added cont_years

--- main.py
+++ main.py
@@ -43,12 +43,14 @@ def user_details():
             user_age = int(post_data['ageval'])
             users_ref = db.collection('users')
             field_filter = FieldFilter("age", "==", user_age)
-            query = users_ref.where(filter=field_filter)  # Your query conditions here
-            results = query.stream()
-            users_data = [user_age]
-            for result in results:
-                users_data.append({"id": result.id, "data": result.to_dict()})
-            return jsonify(users_data)
+            cont_years = Customer.year_diff(c1234, TVNZ Inc, c1, 2021, 3, 10, 10000, 3, 2027, 10, 23, 18)
+            return jsonify(cont_years)
+            # query = users_ref.where(filter=field_filter)  # Your query conditions here
+            # results = query.stream()
+            # users_data = [user_age]
+            # for result in results:
+            #     users_data.append({"id": result.id, "data": result.to_dict()})
+            # return jsonify(users_data)
     else:
         users_data = ["hello there shane"]
         print(users_data)
