18887c3eeea4b5a6d75099922a4dfa412e4dfcdb
suzcode <suzetteloomb@gmail.com>
2023-12-12 20:20:54 +1300

    changed class names

--- contractcalcs.py
+++ contractcalcs.py
@@ -6,18 +6,18 @@ from itertools import accumulate
 
 class Customer:
     def __init__(self, contract_id, customer_id, customer_name, end_day, end_month, end_year, percent_inc, start_day, start_month, start_subs, start_year, term_months):
-        self.contract_id = contract_id
-        self.customer_id = customer_id
-        self.customer_name = customer_name
-        self.end_day = end_day
-        self.end_month = end_month
-        self.end_year = end_year
-        self.percent_inc = percent_inc
-        self.start_day = start_day
-        self.start_month = start_month
-        self.start_subs = start_subs
-        self.start_year = start_year
-        self.term_months = term_months
+        self.conti = contract_id
+        self.custi = customer_id
+        self.custn = customer_name
+        self.endd = end_day
+        self.endm = end_month
+        self.endy = end_year
+        self.perci = percent_inc
+        self.startd = start_day
+        self.startm = start_month
+        self.starts = start_subs
+        self.starty = start_year
+        self.termm = term_months
         return contract_id, customer_id, customer_name, end_day, end_month, end_year, percent_inc, start_day, start_month, start_subs, start_subs, start_year, term_months
 
     def start(self):
@@ -48,23 +48,22 @@ class Customer:
      # creates a list for the term of contract of zero values
     def create_list(self):
         bill_list = []
-        print(self)
         additional_months = 0
-        if self.term_months % 12 == 0:
-            if abs(self.end_day - self.start_day) <= 1:
+        if self.termm % 12 == 0:
+            if abs(self.endd - self.startd) <= 1:
                 # if teh start month is 1 there is no need for a follow on year
-                if self.start_month > 1:
+                if self.startm > 1:
                     additional_months = 12
                     print("addition div ==0", additional_months)
         else:
-            if self.term_months > 12:
-                additional_months = 12 - (self.term_months % 12)
+            if self.termm > 12:
+                additional_months = 12 - (self.termm % 12)
                 print("addtional term > 12", additional_months)
             else:
-                additional_months = 12 - self.term_months
+                additional_months = 12 - self.termm
                 print("addtional term < 12", additional_months)
         # creates a list of zero for the term of the contract
-        for i in range(int(self.term_months + additional_months)):
+        for i in range(int(self.termm + additional_months)):
             bill_list.append(0)
         print('length of bill list', len(bill_list))
         return bill_list
