351bf76e17fd2e48e74896010e04e277def2c43c
suzcode <suzetteloomb@gmail.com>
2023-12-13 21:14:15 +1300

    correct .yr to .year

--- contractcalcs.py
+++ contractcalcs.py
@@ -68,10 +68,10 @@ class Customer:
         return bill_list
 
     def create_yearList(self, billList):
-        yearList = self.start_yr
+        yearList = self.start_year
         i = 1
         while i < (len(billList) / 12):
-            yearList.append(self.start_yr + i)
+            yearList.append(self.start_year + i)
             i += 1
         return yearList
 
