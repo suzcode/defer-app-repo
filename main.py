87266778786042d09228d12189bc76a2334a6fe8
suzcode <suzetteloomb@gmail.com>
2024-01-05 14:22:33 +1300

    removed explicit GET and POST from function

--- main.py
+++ main.py
@@ -185,22 +185,17 @@ def customers():
         CUSTOMERS = customers_data
     return jsonify(CUSTOMERS)
 
-@app.route('/addcontract', methods=['GET', 'POST'])
+@app.route('/addcontract')
 def add_contract():
-    if request.method == 'POST':
-        print('Request', request)
-        if request.data == None or request.data == '':
-            print('null or empty string value for data in a file')
-        else:
-            request_value = json.loads(request.data)
-            # get values from dict
-            new_contract_object = request_value['entries']
-            subscriber_id = 'Charlie Corp'
-            database_ref = db.collection('subscribers')
-            new_contract_ref = database_ref.document(subscriber_id).collection('contracts').document()
-            new_contract_ref.set(new_contract_object)
-            print('OBJECT ADDED', new_contract_object)
-            return new_contract_object
+    request_value = json.loads(request.data)
+    # get values from dict
+    new_contract_object = request_value['entries']
+    subscriber_id = 'Charlie Corp'
+    database_ref = db.collection('subscribers')
+    new_contract_ref = database_ref.document(subscriber_id).collection('contracts').document()
+    new_contract_ref.set(new_contract_object)
+    print('OBJECT ADDED', new_contract_object)
+    return new_contract_object
 
 # retrieve all customer profile info for a selected year 
 @app.route('/yearfilter', methods=['GET', 'POST'])
